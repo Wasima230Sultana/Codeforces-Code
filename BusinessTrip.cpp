f65e0062f59b9c303660e17886833e1cf9616ee5
wasimasultana230 <wasimasultana230@gmail.com>
2025-06-16 07:55:38 +0600

    Business Trip

---
+++ BusinessTrip.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int k;
+    cin >> k;
+    int month[12];
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> month[i];
+    }
+
+    sort(month, month + 12, greater<int>());
+
+    if (k == 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int growth = 0;
+    int needMonth = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        growth += month[i];
+        needMonth++;
+        if (growth >= k)
+        {
+            cout << needMonth << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+    return 0;
+}
