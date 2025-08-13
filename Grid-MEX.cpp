6847a5c901754de77cf632c0f04551364c882512
wasimasultana230 <wasimasultana230@gmail.com>
2025-08-13 23:52:23 +0600

    Grid MEX from codechef

---
+++ Grid-MEX.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << ((i + j) % n) << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
