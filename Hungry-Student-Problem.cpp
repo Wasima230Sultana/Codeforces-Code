8d0b6d04c5bc47b7a7417a5824e380d10717696b
wasimasultana230 <wasimasultana230@gmail.com>
2025-08-13 23:47:15 +0600

    Hungry Student Problem

---
+++ Hungry-Student-Problem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        if (x == 1 || x == 2 || x == 4 || x == 5 || x == 8 || x == 11)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
